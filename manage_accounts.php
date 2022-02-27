0b3fef7f7249ca2f5fb9a470357896346a680784
JP Sison <johnpatrick.sison.iics@ust.edu.ph>
2022-02-27 19:57:27 +0800

    fdf

---
+++ manage_accounts.php
@@ -0,0 +1,25 @@
+<html>
+    <body>
+       <div class="main-content">
+            <div class="wrapper">
+                 <h1>Manage Admin/h1>
+                     <tr>
+                          <th>S.N. </th>
+                          <th>Full Name</Lh>
+                          <th>Username </th>
+                          <th>Actions </th>
+                     </tr>
+                     <tr>
+                          <td>1. </td>
+                          <td>admin</td>
+                           <td>admin</td>
+                          <td>
+                              Update Admin
+                              Delete Admin
+                          </td>
+                     </tr>
+                 </table>
+             </div>
+        </div>
+    </body>
+</html>
