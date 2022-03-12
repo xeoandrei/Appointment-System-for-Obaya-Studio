53f56735f13253883b1d380a53ec4e0dada0df31
Francis Jacob Taiño <francistaino16@gmail.com>
2022-03-12 18:20:34 +0800

    Removed unnecessary css files.

--- book_appointment.php
+++ book_appointment.php
@@ -19,80 +19,12 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
     <!-- CSS -->
-    <link rel="stylesheet" href="css/book_appointment.css">
+    <link rel="stylesheet" href="css/servicesfood.css">
 
     <title>Obaya Studio | Book Appointment</title>
 </head>
 <body>
-    <section id = firstPageMainBG>
-       
-    <section id="firstPage" class="container-fluid"> 
-        <nav class="navbar navbar-expand-lg navbar-dark">
-            <a class="navbar-brand" href="index.php">
-                <img src="images/logo3.png" style="height: 90px; margin-right: 2em;">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">Home</a>
-                    </li>
-        
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" href="#">
-                            Services
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-                            <li>
-                                <a class="dropdown-item" href="services_men.html">For Men</a>
-                            </li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li>
-                                <a class="dropdown-item" href="services_women.html">For Women</a>
-                            </li>
-                        </ul>
-                    </li>
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" href="#">
-                            Book
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-                            <li>
-                                <a class="dropdown-item" href="verify_appointment.html">Book Appointment</a>
-                            </li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li>
-                                <a class="dropdown-item" href="#">Cancel Appointment</a>
-                            </li>
-                        </ul>
-                    </li>
-        
-                    <li class="nav-item">
-                        <a class="nav-link" href="food.html">Food</a>
-                    </li>
-        
-                    <li class="nav-item">
-                        <a class="nav-link" href="about_us.html">About Us</a>
-                    </li>
-
-                </ul>
-            </div>
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link d-lg-none" href="login.php">
-                            Log-In
-                        </a>
-                        <a class="nav-link d-none d-lg-block" href="login.php">
-                            <img src="images/user.png" style="height:40px;" alt="">
-                        </a>
-                    </li>
-                </ul>
-            </div>  
-    </nav>
+    <?php include "navbar/navbar.php"; ?>
 
         <div class="marginTop">
             <h1>Book Appointment</h1>
