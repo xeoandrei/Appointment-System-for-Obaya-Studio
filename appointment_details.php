eaf4ad184a08514ad04fb357b29e77d00c3bcf2c
Francis Jacob Taiño <francistaino16@gmail.com>
2022-03-24 23:22:23 +0800

    Dashboard can now count number of items in a row.

--- appointment_details.php
+++ appointment_details.php
@@ -1,6 +1,13 @@
 <?php
     include 'connection.php';
     session_start();
+
+    if(isset($_SESSION['email']) AND ($_SESSION['usertype'] == 'ADMINISTRATOR')){
+        echo 'Good day! ' . $_SESSION['email'] . ' <a href="management.php">Admin Panel</a>';
+    } elseif(isset($_SESSION['email']) AND ($_SESSION['usertype'] == 'STAFF')) {
+        echo 'Good day! ' . $_SESSION['email'] . ' <a href="management.php">Staff Panel</a>';
+    }
+
     if(isset($_POST['viewAppointmentSubmit'])){
         $tokenId = $_POST['tokenId'];
         //Check if token id exists
---
+++ dashboard_counter.php
@@ -0,0 +1,49 @@
+<?php
+
+    //Count # of appointments.
+    $sql = "SELECT * from appointment";
+
+    if ($result = mysqli_query($con, $sql)) {
+
+        // Return the number of rows in result set
+        $appointmentRow = mysqli_num_rows( $result );
+        
+    } else {
+        $appointmentRow = 0;
+    }
+
+    //Count # of pending appointments.
+    $sql = "SELECT * from appointment WHERE status='Pending'";
+
+    if ($result = mysqli_query($con, $sql)) {
+
+        // Return the number of rows in result set
+        $pendingAppointmentRow = mysqli_num_rows( $result );
+        
+    } else {
+        $pendingAppointmentRow = 0;
+    }
+
+    //Count # of verified appointments.
+    $sql = "SELECT * from appointment WHERE status='Verified'";
+
+    if ($result = mysqli_query($con, $sql)) {
+
+        // Return the number of rows in result set
+        $verifiedAppointmentRow = mysqli_num_rows( $result );
+        
+    } else {
+        $verifiedAppointmentRow = 0;
+    }
+
+    //Count # of user accounts.
+    $sql = "SELECT * from usertable";
+
+    if ($result = mysqli_query($con, $sql)) {
+
+        // Return the number of rows in result set
+        $userRow = mysqli_num_rows( $result );
+        
+    } else {
+        $userRow = 0;
+    }
