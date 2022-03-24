e1517c9350ba3615b360853eccdec38c1d2d9640
Francis Jacob Taiño <francistaino16@gmail.com>
2022-03-24 23:39:54 +0800

    Changed token Id length to 12.

--- submit_booking.php
+++ submit_booking.php
@@ -5,7 +5,9 @@
     //Generate token id.
     function generateCode($limit){
         $code = '';
-        for($i = 0; $i < $limit; $i++) { $code .= mt_rand(0, 9); }
+        for($i = 0; $i < $limit; $i++) {
+            $code .= mt_rand(0, 9);
+        }
         return $code;
         }
 
