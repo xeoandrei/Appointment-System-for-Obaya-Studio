03ca3848c8bb41abbfa1b2bd5b4ebcf4064fe29e
Samarth Sharma <samarth.sharma.iics@ust.edu.ph>
2022-04-19 23:28:50 +0800

    get name changed to get id for delete accounts button

--- delete_account.php
+++ delete_account.php
@@ -1,10 +1,10 @@
 <?php
 require_once "config.php";
 include("session-checker.php");
-if(isset($_GET['name'])){
-	$sql = "DELETE FROM usertable WHERE name = ?";
+if(isset($_GET['id'])){
+	$sql = "DELETE FROM usertable WHERE id = ?";
 	if($stmt = mysqli_prepare($link, $sql)){
-		mysqli_stmt_bind_param($stmt, "s", trim($_GET["name"]));
+		mysqli_stmt_bind_param($stmt, "s", trim($_GET["id"]));
 		if(mysqli_stmt_execute($stmt)){
 			$_SESSION['delete-account']="User Account Deleted!";
             header("location: manage_accounts.php");
