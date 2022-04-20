deb3f9743103630fd7c2ac4ad650d6369cf3e858
Samarth Sharma <samarth.sharma.iics@ust.edu.ph>
2022-04-20 14:29:43 +0800

    fixed footer

--- footer/footer.php
+++ footer/footer.php
@@ -1,6 +1,6 @@
 <section id="footer" class="py-4">
     <div class="sticky-bottom">
-        <a class="mx-2 my-3"href="https://www.facebook.com/messages/t/108422251306606" style="color:#959fa3;"><i class="fab fa-facebook-messenger"></i></a>
+        <a class="mx-2 my-3" href="https://www.facebook.com/messages/t/108422251306606" style="color:#959fa3;"><i class="fab fa-facebook-messenger"></i></a>
         <a class="mx-2 my-3" href="https://www.facebook.com/obayastudio" style="color:#959fa3;"><i class="fab fa-facebook-f"></i></a>
         <a class="mx-2 my-3" href="https://www.instagram.com/obayastudio/" style="color:#959fa3;"><i class="fab fa-instagram"></i></a>
         <a class="mx-2 my-3" href="mailto:team.obayastudio@gmail.com" style="color:#959fa3;"><i class="fas fa-envelope"></i></a>
