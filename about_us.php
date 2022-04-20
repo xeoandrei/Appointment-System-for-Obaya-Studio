415edefc19c915905eae40201304a4ebaf030614
Andrei <xeoandrei@gmail.com>
2022-04-20 18:19:43 +0800

    added carousel on index and about us pages

--- about_us.php
+++ about_us.php
@@ -28,30 +28,102 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
     <!-- CSS -->
-    <link rel="stylesheet" href="css/servicesfood.css">
-
+    <link rel="stylesheet" href="css/index.css?v=<?php echo time(); ?>">
+    
     <title>Obaya Studio | About Us</title>
 </head>
+
 <body>
-    <?php include "navbar/navbar.php"; ?>
-        <div class="mt-5">
-            <h1 class="firstPageText">About Us!</h1>
-            <div class="container">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra suspendisse potenti nullam ac tortor. Massa sapien faucibus et molestie ac feugiat sed lectus. Morbi non arcu risus quis varius quam quisque id diam. Integer eget aliquet nibh praesent tristique. Et egestas quis ipsum suspendisse. Eget nunc lobortis mattis aliquam faucibus purus in massa. Ullamcorper morbi tincidunt ornare massa eget egestas purus. Mattis vulputate enim nulla aliquet porttitor lacus. Mauris pharetra et ultrices neque. Lectus urna duis convallis convallis tellus id interdum velit laoreet. Ridiculus mus mauris vitae ultricies leo integer. Cras tincidunt lobortis feugiat vivamus at. Urna porttitor rhoncus dolor purus non enim praesent. Cras adipiscing enim eu turpis egestas pretium aenean pharetra. Eget arcu dictum varius duis at consectetur. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula. Etiam erat velit scelerisque in.
+       
+    <section id="firstPage" class="container-fluid"> 
+        <?php include "navbar/navbar-index.php"; ?>
 
-Lobortis elementum nibh tellus molestie nunc non blandit massa. Libero enim sed faucibus turpis in eu mi bibendum neque. Massa placerat duis ultricies lacus sed. Fames ac turpis egestas integer eget aliquet nibh praesent tristique. Amet porttitor eget dolor morbi non arcu risus quis. Sem fringilla ut morbi tincidunt. Justo eget magna fermentum iaculis eu non diam phasellus vestibulum. Dui vivamus arcu felis bibendum ut tristique et egestas quis. Aliquam ut porttitor leo a diam sollicitudin tempor. Commodo viverra maecenas accumsan lacus vel facilisis.
-
-Eu augue ut lectus arcu bibendum. In nulla posuere sollicitudin aliquam ultrices sagittis orci a. Lobortis feugiat vivamus at augue eget arcu dictum. Justo donec enim diam vulputate ut pharetra sit amet. Venenatis cras sed felis eget. Et netus et malesuada fames ac turpis. Libero enim sed faucibus turpis in eu mi. Placerat duis ultricies lacus sed turpis tincidunt id aliquet. Diam volutpat commodo sed egestas egestas fringilla phasellus. Tristique senectus et netus et malesuada fames. Cras sed felis eget velit. Neque ornare aenean euismod elementum nisi quis eleifend. Aliquet eget sit amet tellus cras adipiscing enim. Et netus et malesuada fames ac.
-
-Est placerat in egestas erat imperdiet sed. Nulla at volutpat diam ut. Dis parturient montes nascetur ridiculus. Quis commodo odio aenean sed adipiscing diam donec adipiscing tristique.</p>
+        <div class="row marginTop">
+            <div class="col-lg-6">
+                    <h1 class="fw-bold whiteText">About Us</h1>
+                        <div class="mt-3">
+                        <p class="whiteText">
+                        The origin of the word Obaya can be traced back to Sagada. It means a period of rest, 
+                        a sacred day for resting to bless the coming season of harvest. 
+                        Relaxation is an important aspect in the life of people. We used the word as 
+                        the cornerstone of our cause, to deliver relaxation at its highest form, through hair cosmetology. </p>
+                        </div>
+                        <div class="whiteText">
+                        Although the initial plan was to open a hair studio, our fondness for coffee sparked an idea 
+                        of combining the two concepts. Hence, Obaya Studio Salon and Coffee Bar was made, making a venue 
+                        where we can serve the ultimate form of relaxation through Hair Cosmetology with the Art of Espresso.
+                        We are deeply rooted in delivering the signature “Obaya Experience” to our clients. 
+                        It’s more than just styling hair and making drinks, it’s the signature experience that we deliver. 
+                        </div>
+                        <div class="whiteText mt-2"> 
+                        Here at Obaya Studio, expect only the best. 
+                        </div>           
+                            <form>
+                                <a href="feedback_verify.php" class="btn btn-outline-light btn-lg my-5"><i class="fas fa-book"></i>
+                                    Send Feedback
+                                </a>
+                            </form>  
+            </div>        
+            <div class="col-lg-6 mobMargin">
+            <div class="card">
+                    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                            <img src="images/AboutUsImages/Square1.jpeg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                            <img src="images/AboutUsImages/Square2.jpeg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                            <img src="images/AboutUsImages/Square3.jpeg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                            <img src="images/AboutUsImages/Square4.jpeg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                            <img src="images/AboutUsImages/Square5.jpeg" class="d-block w-100" alt="...">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                        </div>
+                    </div>
             </div>
-            
-            <form>
-                <a href="feedback_verify.php" class="btn btn-outline-dark btn-lg my-5"><i class="fas fa-book"></i>
-                    Send Feedback
-                </a>
-            </form>
         </div>
+
+        <div class="row mt-4">
+            <div class="col-lg-6">
+            <h1 class="fw-bold whiteText">Contact Us</h1>
+                <div class="mt-3">
+                        <p class="whiteText">
+                        <i class="fas fa-location-arrow"></i> Address: Excelsis Building, 6H5H+RJP, Mabalacat, Pampanga
+                        </p>
+                        <p class="whiteText">
+                        <i class="fas fa-hourglass"></i> Hours: Opens at 8AM ⋅ Closes at 6PM
+                        </p>
+                        <p class="whiteText">
+                        <i class="fas fa-phone"></i> Phone: 0906 404 2623
+                        </p>
+                        <p class="whiteText">
+                        <i class="fas fa-envelope"></i> Email: <a class="whiteText text-decoration-none" href="mailto:team.obayastudio@gmail.com" > team.obayastudio@gmail.com</a>
+                        </p>
+                        <p class="whiteText">
+                        <i class="fab fa-facebook-messenger"></i> Messenger: <a class="whiteText text-decoration-none" text-decoration: none; href="https://www.facebook.com/messages/t/108422251306606"> Obaya Studio </a>
+                        </p>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                    <h1 class="fw-bold whiteText">Location</h1>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15400.26112807597!2d120.579074!3d15.2095934!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa2bb8477c37b249!2sObaya%20Studio%20Salon%20%2B%20Coffee%20Bar!5e0!3m2!1sen!2sph!4v1650446197789!5m2!1sen!2sph" width="100%" height="80%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </section>
     <?php include "footer/footer.php"; ?>     
 </body>
 </html>
--- index.php
+++ index.php
@@ -49,7 +49,28 @@
                 </form>           
             </div>
             <div class="col-lg-6 mobMargin">
-                <img class="titlePic" src="images/titlepic.jpg" alt="">
+            <div class="card">
+                    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                            <img src="images/titlepic2.jpeg" class="d-block w-100 titlePic " alt="...">
+                            </div>
+                            <div class="carousel-item">
+                            <img src="images/titlepic3.jpeg" class="d-block w-100 titlePic " alt="...">
+                            </div>
+                            <div class="carousel-item">
+                            <img src="images/titlepic4.jpeg" class="d-block w-100 titlePic " alt="...">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
             </div>
         </div>
     </section>
