817724a7731d91bc35454d05b47641d7373385d4
Andrei <xeoandrei@gmail.com>
2022-04-20 11:55:38 +0800

    added footer links

--- footer/footer.php
+++ footer/footer.php
@@ -1,9 +1,9 @@
 <section id="footer" class="py-4">
     <div class="sticky-bottom">
-        <a class="mx-2 my-3"href="" style="color:#959fa3;"><i class="fab fa-twitter"></i></a>
-        <a class="mx-2 my-3" href="" style="color:#959fa3;"><i class="fab fa-facebook-f"></i></a>
-        <a class="mx-2 my-3" href="" style="color:#959fa3;"><i class="fab fa-instagram"></i></a>
-        <a class="mx-2 my-3" href="" style="color:#959fa3;"><i class="fas fa-envelope"></i></a>
+        <a class="mx-2 my-3"href="https://www.facebook.com/messages/t/108422251306606" style="color:#959fa3;"><i class="fab fa-facebook-messenger"></i></a>
+        <a class="mx-2 my-3" href="https://www.facebook.com/obayastudio" style="color:#959fa3;"><i class="fab fa-facebook-f"></i></a>
+        <a class="mx-2 my-3" href="https://www.instagram.com/obayastudio/" style="color:#959fa3;"><i class="fab fa-instagram"></i></a>
+        <a class="mx-2 my-3" href="mailto:team.obayastudio@gmail.com" style="color:#959fa3;"><i class="fas fa-envelope"></i></a>
         <p class="mx-2 my-3">© Copyright 2021 Obaya</p>
     </div>
 </section>
