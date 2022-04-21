b6690ab6c6ddc2aa34e257faa054e1ffa1a8116a
Samarth Sharma <samarth.sharma.iics@ust.edu.ph>
2022-04-21 23:17:37 +0800

    fixed email for notifications

--- notify_cancelled.php
+++ notify_cancelled.php
@@ -4,11 +4,26 @@ include_once('connection.php');
 if(isset($_GET['notifcancel'])){
     $id = $_GET['id'];
 	$email = $_GET['email'];
-
-    $subject = "Obaya Notification for Cancelled Appointment";
-    $message = "Your Token Id is $id." . " Your appointment status is now Cancelled.";
+    
     $sender = "From: sammygarma26@gmail.com";
-    if(mail($email, $subject, $message, $sender)){
+    $subject = "Obaya Notification for Cancelled Appointment";
+
+    // To send HTML mail, the Content-type header must be set
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+    // Create email headers
+    $headers .= 'From: '.$sender."\r\n".
+    'Reply-To: '.$sender."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+    
+    // Compose a simple HTML email message
+    $message = '<html><body>';
+    $message .= '<p>Good day! We are very sorry but your appointment with the Token ID: '. "$id," . ' did not went as scheduled.</p>';
+    $message .= '</body></html>';
+
+    if(mail($email, $subject, $message, $headers)){
         $_SESSION['email'] = $email;
         header('location: ' . $_SERVER['HTTP_REFERER']);
     }
--- notify_verified.php
+++ notify_verified.php
@@ -5,13 +5,28 @@ if(isset($_GET['notifverify'])){
     $id = $_GET['id'];
 	$email = $_GET['email'];
 
-    $subject = "Obaya Notification for Confirmed Appointment";
-    $message = "Your Token Id is $id." . " Your appointment status is now Confirmed.";
     $sender = "From: sammygarma26@gmail.com";
-    if(mail($email, $subject, $message, $sender)){
+    $subject = "Obaya Notification for Confirmed Appointment";
+
+    // To send HTML mail, the Content-type header must be set
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+    // Create email headers
+    $headers .= 'From: '.$sender."\r\n".
+    'Reply-To: '.$sender."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+    
+    // Compose a simple HTML email message
+    $message = '<html><body>';
+    $message .= '<p style="color:black;">Good day! Your appointment has been confirmed. You can view your appointment details ' . '<a href="http://localhost/Obaya-Studio/view_appointment.php">here</a>' . ' by entering your Token ID:' . " $id." . '</p>';
+    $message .= '</body></html>';
+
+    if(mail($email, $subject, $message, $headers)){
         $_SESSION['email'] = $email;
         header('location: ' . $_SERVER['HTTP_REFERER']);
-    }
+    }   
     else
     {
         $errors['otp-error'] = "Failed while sending notification!";
