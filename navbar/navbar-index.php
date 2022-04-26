a21882b8855a3ec877056d4a10929661dc9f5dd8
Andrei <xeoandrei@gmail.com>
2022-04-26 10:52:07 +0800

    working hover

--- navbar/navbar-index.php
+++ navbar/navbar-index.php
@@ -7,7 +7,7 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
                     <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
+                        <li class="nav-item dropdown">
                             <a class="nav-link fw-bold" href="index.php">Home</a>
                         </li>
             
@@ -41,11 +41,11 @@
                             </ul>
                         </li>
             
-                        <li class="nav-item">
+                        <li class="nav-item dropdown">
                             <a class="nav-link" href="food.php">Food</a>
                         </li>
             
-                        <li class="nav-item">
+                        <li class="nav-item dropdown">
                             <a class="nav-link" href="about_us.php">About Us</a>
                         </li>
 
@@ -73,12 +73,32 @@
                 </div>  
         </nav>
         
-        <style>
+<style>
         @media all and (min-width: 992px) {
     .navbar .nav-item:hover .nav-link{ color: #FFF;;  }       
 	.navbar .dropdown-menu.fade-down{ top:80%; transform: rotateX(-75deg); transform-origin: 0% 0%;}
 	.navbar .dropdown-menu.fade-up{ top:180%;  }
 	.navbar .nav-item:hover .dropdown-menu{ transition: .3s; opacity:1; visibility:visible; top:100%; transform: rotateX(0deg); color: #d48b33; }
+    *,*:before,*:after{
+        padding:0;
+        margin: 0;
+        box-sizing: border-box;
+    }
 
+    .navbar .nav-item:after{
+        content:"";
+        position: absolute;
+        background-color: white;
+        height: 3px;
+        width: 0;
+        left:0;
+  
+        transition: .5s;
+    }
+  
+    .navbar .nav-item:hover:after{
+        width:100%;
+    }  
+    
 }	
 </style>
